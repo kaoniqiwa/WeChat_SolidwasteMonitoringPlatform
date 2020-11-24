11fbdd805e8230c79d0b1279dcddfc434b9b0c1d
swb <495146161@qq.com>
2020-11-24 10:25:52 +0800

    lib

---
+++ app/data-core/repuest/http-client.ts
@@ -0,0 +1,15 @@
+
+import { HowellAuthHttp } from "./howell-auth-http";
+export namespace HowellHttpClient {
+
+    export class HttpClient {
+
+        constructo() {
+
+        }
+
+        get http() {
+            return new HowellAuthHttp('null', 'null');
+        }
+    }
+}
