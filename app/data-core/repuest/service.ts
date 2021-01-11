9fae9e3c9f4cdf2a3c68b4804764bdd50f747e18
zzl <kila_z@163.com>
2021-01-11 16:55:43 +0800

    add role

--- app/data-core/repuest/service.ts
+++ app/data-core/repuest/service.ts
@@ -14,15 +14,25 @@ export class Service {
 
 
     private _user?: WeChatRequestService;
-    // 用户信息服务
+    /** 用户信息服务 */
     get user(): WeChatRequestService {
         if (!this._user) {
             this._user = new WeChatRequestService(this.requestService);
         }
         return this._user;
     }
+    /** 角色信息服务 */
+    private _role?: RoleRequestService;
+    get role(): RoleRequestService {
+        if (!this._role) {
+            this._role = new RoleRequestService(this.requestService);
+        }
+        return this._role;
+    }
+
+
     private _garbageStation?: GarbageStationRequestService;
-    // 垃圾厢房服务
+    /** 垃圾厢房服务 */
     get garbageStation() {
         if (!this._garbageStation) {
             this._garbageStation = new GarbageStationRequestService(this.requestService);
@@ -31,7 +41,7 @@ export class Service {
     }
     // 
     private _division?: DivisionRequestService;
-    // 区划信息服务
+    /** 区划信息服务 */
     get division() {
         if (!this._division) {
             this._division = new DivisionRequestService(this.requestService);
@@ -40,7 +50,7 @@ export class Service {
     }
 
     private _camera?: CameraRequestService;
-    // 摄像机信息服务
+    /** 摄像机信息服务 */
     get camera() {
         if (!this._camera) {
             this._camera = new CameraRequestService(this.requestService);
@@ -49,7 +59,7 @@ export class Service {
     }
 
     private _media?: Mediume;
-    // 媒体服务
+    /** 媒体服务 */
     get media(): Mediume {
         if (!this._media) {
             this._media = new Mediume();
