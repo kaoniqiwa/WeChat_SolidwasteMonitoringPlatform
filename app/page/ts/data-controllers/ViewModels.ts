5134cd73fbac3e45979871ddb35e5bf75c0c4c06
tuzhongyi <tuzhongyi@howell.net.cn>
2021-04-07 09:11:33 +0800

    debug

--- app/page/ts/data-controllers/ViewModels.ts
+++ app/page/ts/data-controllers/ViewModels.ts
@@ -2,23 +2,13 @@ import { GarbageStation } from "../../../data-core/model/waste-regulation/garbag
 import { GarbageStationNumberStatistic } from "../../../data-core/model/waste-regulation/garbage-station-number-statistic";
 
 export class GarbageStationViewModel extends GarbageStation {
-    NumberStatistic: GarbageStationNumberStatistic
+    NumberStatistic?: GarbageStationNumberStatistic
 }
 
 export class ViewModelConverter {
     static Convert(model: GarbageStation): GarbageStationViewModel {
-        let vm = new GarbageStationViewModel();
-        vm.Id = model.Id;
-        vm.MaxDryVolume = model.MaxDryVolume;
-        vm.MaxWetVolume = model.MaxWetVolume;
-        vm.Name = model.Name;
-        vm.StationState = model.StationState;
-        vm.StationType = model.StationType;
-        vm.TrashCans = model.TrashCans;
-        vm.UpdateTime = model.UpdateTime;
-        vm.WetFull = model.WetFull;
-        vm.WetFullTime = model.WetFullTime;
-        vm.WetVolume = model.WetVolume;
-        return vm;
+        if (model instanceof GarbageStation) {
+            return Object.assign(GarbageStationViewModel, model);
+        }        
     }
 }
