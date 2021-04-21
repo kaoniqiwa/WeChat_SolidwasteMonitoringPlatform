37207820b471fa7c40cc5f02f97d1b7f313dfa51
panminxiang <1061886912@qq.com>
2021-04-21 15:54:53 +0800

    垃圾落地

--- app/page/ts/garbage-drop_pmx.ts
+++ app/page/ts/garbage-drop_pmx.ts
@@ -1,5 +1,6 @@
 import { NavigationWindow } from ".";
+import "../css/header.less"
+import "../css/garbage-drop.css"
 
 const user = (window.parent as NavigationWindow).User;
 const http = (window.parent as NavigationWindow).Authentication;
-
